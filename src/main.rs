63cf2e733e4dd1762fd53074627a602469976819
Aaryan Pashine <pashineaaryan@gmail.com>
2023-07-19 16:54:15 -0400

    sparkly start

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ struct KeyPressed {
 }
 
 const LIFETIME: i32 = 400;
+const SCALE: f32 = 3.;
 
 fn main() {
     App::new()
@@ -22,7 +23,7 @@ fn main() {
             ..default()
         })
         .insert_resource(KeyPressed { pressed: false })
-        .insert_resource(ClearColor(Color::rgb(1.0, 1.0, 1.0)))
+        .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
         .add_plugins(DefaultPlugins)
         .add_plugins(TemporalAntiAliasPlugin)
         .add_plugins(EguiPlugin)
@@ -43,7 +44,7 @@ fn setup(mut commands: Commands) {
                 hdr: true,
                 ..default()
             },
-            transform: Transform::from_translation(Vec3::new(0.0, 100.0, 15.0))
+            transform: Transform::from_translation(Vec3::new(0.0, 100.0, 15.0) * SCALE)
                 .looking_at(Vec3::default(), Vec3::Y),
             ..default()
         })
@@ -61,10 +62,10 @@ fn setup(mut commands: Commands) {
     commands.spawn(DirectionalLightBundle {
         directional_light: DirectionalLight {
             shadows_enabled: true,
-            illuminance: 100.,
+            illuminance: 50.,
             ..default()
         },
-        transform: Transform::from_translation(Vec3::new(0.0, 100.0, 15.0))
+        transform: Transform::from_translation(Vec3::new(300.0, 1.0, 15.0) * SCALE)
             .looking_at(Vec3::default(), Vec3::Y),
         ..Default::default()
     });
@@ -81,25 +82,14 @@ fn keyboard_input_system(keyboard_input: Res<Input<KeyCode>>, mut pressed: ResMu
 #[derive(Component)]
 struct AutoCube {
     life_time: i32,
-}
-
-#[derive(Component)]
-struct AutoSphere {
-    life_time: i32,
-}
-
-impl Default for AutoSphere {
-    fn default() -> Self {
-        AutoSphere {
-            life_time: LIFETIME,
-        }
-    }
+    bounds: Vec3,
 }
 
 impl Default for AutoCube {
     fn default() -> Self {
         AutoCube {
             life_time: LIFETIME,
+            bounds: Vec3::new(10.0, 10.0, 10.0) * SCALE,
         }
     }
 }
@@ -125,15 +115,17 @@ fn spawn_block(
                 reflectance: 0.1,
                 ..default()
             }),
-            transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
+            transform: Transform::from_translation(Vec3::new(0.0, 10.0, 0.0) * SCALE),
             ..default()
         })
-        .insert(AutoCube::default());
+        .insert(AutoCube {
+            bounds: Vec3::new(2.0, 8.0, 2.0) * SCALE,
+            ..default()
+        });
     commands
         .spawn(PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::UVSphere {
-                radius: rand::thread_rng().gen_range(0.01..0.5),
-                ..Default::default()
+            mesh: meshes.add(Mesh::from(shape::Cube {
+                size: rand::thread_rng().gen_range(0.01 * SCALE..0.5 * SCALE),
             })),
             material: materials.add(StandardMaterial {
                 base_color: Color::rgb(1.0, 0.7, 0.0),
@@ -142,10 +134,13 @@ fn spawn_block(
                 reflectance: 0.1,
                 ..default()
             }),
-            transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
+            transform: Transform::from_translation(Vec3::new(4.0, 2.0, 2.0 * SCALE)),
             ..default()
         })
-        .insert(AutoSphere::default());
+        .insert(AutoCube {
+            bounds: Vec3::new(8.0, 2.0, 2.0) * SCALE,
+            ..default()
+        });
 }
 
 fn update_block(
@@ -153,74 +148,46 @@ fn update_block(
     mut materials: ResMut<Assets<StandardMaterial>>,
     mut commands: Commands,
     mut blocks: Query<(Entity, &mut AutoCube, &Transform, &Handle<StandardMaterial>)>,
-    mut speheres: Query<(
-        Entity,
-        &mut AutoSphere,
-        &Transform,
-        &Handle<StandardMaterial>,
-    )>,
 ) {
-    for (entity, mut block, transform, material) in speheres.iter_mut() {
-        if block.life_time == LIFETIME {
-            commands
-                .spawn(PbrBundle {
-                    mesh: meshes.add(Mesh::from(shape::UVSphere {
-                        radius: rand::thread_rng().gen_range(0.01..0.5),
-                        ..Default::default()
-                    })),
-                    material: materials.add(StandardMaterial {
-                        base_color: Color::rgb(1., 1.0, 1.0),
-                        emissive: Color::rgb(0.2, 0.2, 0.2),
-                        ..default()
-                    }),
-                    transform: Transform::from_translation(get_random_direction(
-                        transform.translation,
-                    )),
-                    ..Default::default()
-                })
-                .insert(AutoSphere { ..default() });
-        }
-        let life_percent = block.life_time as f32 / LIFETIME as f32;
-
-        let m = StandardMaterial {
-            base_color: Color::rgb(life_percent, life_percent, life_percent),
-            emissive: Color::rgb(0.2, 0.2, 0.2),
-            perceptual_roughness: 0.8,
-            ..default()
-        };
-        let _ = materials.set(material, m);
-
-        block.life_time -= 1;
-        if block.life_time == 0 {
-            commands.get_entity(entity).unwrap().despawn_recursive();
-        }
-    }
     // let mut rng = rand::thread_rng();
     for (entity, mut block, transform, material) in blocks.iter_mut() {
         if block.life_time == LIFETIME {
+            let mut r = rand::thread_rng();
+            let r: f32 = r.gen_range(-90.0..90.0);
+            let random_rotation = Quat::from_euler(
+                EulerRot::XYZ,
+                (0.0_f32).to_radians(),
+                (r).to_radians(),
+                (r).to_radians(),
+            );
             commands
                 .spawn(PbrBundle {
                     mesh: meshes.add(Mesh::from(shape::Cube {
-                        size: rand::thread_rng().gen_range(0.01..0.5),
+                        size: rand::thread_rng().gen_range(0.01 * SCALE..0.1 * SCALE),
                     })),
                     material: materials.add(StandardMaterial {
                         base_color: Color::rgb(1., 1.0, 1.0),
                         emissive: Color::rgb(0.2, 0.2, 0.2),
                         ..default()
                     }),
-                    transform: Transform::from_translation(get_random_direction(
-                        transform.translation,
-                    )),
+                    transform: Transform {
+                        translation: get_random_direction(transform.translation, block.bounds),
+                        rotation: random_rotation,
+                        ..default()
+                    },
                     ..Default::default()
                 })
-                .insert(AutoCube { ..default() });
+                .insert(AutoCube {
+                    bounds: block.bounds,
+                    ..default()
+                });
         }
         let life_percent = block.life_time as f32 / LIFETIME as f32;
 
         let m = StandardMaterial {
-            base_color: Color::rgb(life_percent, life_percent, life_percent),
-            emissive: Color::rgb(0.2, 0.2, 0.2),
-            perceptual_roughness: 0.8,
+            base_color: Color::rgb(life_percent, 1., 1.),
+            emissive: Color::rgb(0.1, life_percent, 0.1),
+            perceptual_roughness: 0.3,
             ..default()
         };
         let _ = materials.set(material, m);
@@ -232,20 +199,18 @@ fn update_block(
     }
 }
 
-fn get_random_direction(cur: Vec3) -> Vec3 {
-    let x = get_random_f32(cur.x);
-    let y = get_random_f32(cur.y);
-    let z = get_random_f32(cur.z);
+fn get_random_direction(cur: Vec3, bounds: Vec3) -> Vec3 {
+    let x = get_random_f32(cur.x, bounds.x);
+    let y = get_random_f32(cur.y, bounds.y);
+    let z = get_random_f32(cur.z, bounds.z);
     Vec3::new(x, y, z)
 }
 
-const BORDER: f32 = 10.;
-
-fn get_random_f32(c: f32) -> f32 {
-    let range: f32 = rand::thread_rng().gen_range(-1.0..1.0);
+fn get_random_f32(c: f32, bound: f32) -> f32 {
+    let range: f32 = rand::thread_rng().gen_range(-5.0 * SCALE..5.0 * SCALE);
     match c + range {
-        y if y > BORDER => BORDER,
-        y if y < -BORDER => -BORDER,
+        y if y > bound => bound,
+        y if y < -bound => -bound,
         _ => c + range,
     }
 }
